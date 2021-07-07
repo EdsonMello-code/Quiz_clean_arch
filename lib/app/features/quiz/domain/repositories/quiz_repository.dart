8b0422ef2adf81a6c7ab8f72def6dca831b120f4
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-07 11:41:36 -0300

    feat: Added repository interface of method listQuizes

--- lib/app/features/quiz/domain/repositories/quiz_repository.dart
+++ lib/app/features/quiz/domain/repositories/quiz_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:quiz/app/features/quiz/domain/DTOs/QuizDTO.dart';
+import 'package:quiz/app/features/quiz/domain/entities/quiz_entity.dart';
 import 'package:quiz/app/features/quiz/domain/errors/quiz_error.dart';
 
 abstract class QuizRepository {
   Future<Either<QuizError, void>> createQuiz(QuizDTO params);
+  Future<Either<QuizError, List<QuizEntity>>> listQuizes();
 }
