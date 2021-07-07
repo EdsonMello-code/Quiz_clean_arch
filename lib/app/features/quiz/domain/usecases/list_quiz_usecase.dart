a5a1034b171cacc6f7cc60eee2b379855ad6e625
EdsonMello-code <edsonmelo754@gmail.com>
2021-07-07 11:39:44 -0300

    Added usecase for list quiz

---
+++ lib/app/features/quiz/domain/usecases/list_quiz_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:quiz/app/features/quiz/domain/entities/quiz_entity.dart';
+import 'package:quiz/app/features/quiz/domain/errors/quiz_error.dart';
+import 'package:quiz/app/features/quiz/domain/repositories/quiz_repository.dart';
+
+abstract class IListQuizUsecase {
+  Future<Either<QuizError, List<QuizEntity>>> call();
+}
+
+class ListQuizUsecase implements IListQuizUsecase {
+  final QuizRepository quizRepository;
+
+  ListQuizUsecase({required this.quizRepository});
+  @override
+  Future<Either<QuizError, List<QuizEntity>>> call() async {
+    final quizes = await quizRepository.listQuizes();
+    return quizes;
+  }
+}
